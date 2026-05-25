954ef963b6545e73956893e4cc776cb80f4523fd
Isi Name <perpetualokan5@gmail.com>
2026-05-25 18:12:32 +0100

    Use simple vite config

--- vite.config.js
+++ vite.config.js
@@ -9,14 +9,5 @@ export default defineConfig({
   ],
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react/jsx-runtime'],
-          three: ['three']
-        }
-      }
-    }
   }
 })
